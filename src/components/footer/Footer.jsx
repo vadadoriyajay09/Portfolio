cb3a0192ccec30b059ab3a5538837dc82f262fb9
vadadoriyajay09 <117710045+vadadoriyajay09@users.noreply.github.com>
2023-09-14 13:58:39 +0530

    change contect page

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -23,7 +23,7 @@ const Footer = () => {
           <a
             data-aos="fade-up"
             data-aos-duration="1200"
-            href=""
+            href="https://api.whatsapp.com/send/?phone=917046292419&text=Chat+open+by+https%3A%2F%2Fwhatsappdialer.otutom.com%2Fen%2F&type=phone_number&app_absent=0"
             className="box font-medium text-white  flex items-center justify-center flex-col"
           >
             <IoLogoWhatsapp className=" text-black text-3xl hover:scale-125 cursor-pointer" />
@@ -32,7 +32,7 @@ const Footer = () => {
           <a
             data-aos="fade-up"
             data-aos-duration="1400"
-            href=""
+            href="https://instagram.com/vadadoriyajay2002?utm_source=qr&igshid=MThlNWY1MzQwNA=="
             className="box font-medium text-white  flex items-center justify-center flex-col"
           >
             <RiInstagramFill className=" text-black text-3xl hover:scale-125 cursor-pointer" />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,12 +14,12 @@ const Home = () => {
   }
   return (
     <>
-      <NavBar  darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      <NavBar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
       <Hero darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
       <Skills darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
-      <Work darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      {/* <Work darkMode={darkMode} toggleDarkMode={toggleDarkMode} /> */}
       <Resume darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
-      <Contact darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      {/* <Contact darkMode={darkMode} toggleDarkMode={toggleDarkMode} /> */}
       <Footer darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
     </>
   );
